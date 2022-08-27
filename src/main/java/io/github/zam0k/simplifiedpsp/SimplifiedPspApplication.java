5cad866647de2d721d37339bb351b7595d440876
Kelly Castelo <kawokkun@gmail.com>
2022-08-27 10:40:10 -0300

    initial commit

---
+++ src/main/java/io/github/zam0k/simplifiedpsp/SimplifiedPspApplication.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.simplifiedpsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/zam0k/simplifiedpsp/SimplifiedPspApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.simplifiedpsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
