f049efec1cff3f7406e0b3a3512cf071e1c12ad0
Kelly Castelo <kawokkun@gmail.com>
2022-08-27 11:39:49 -0300

    change person id type to Long for the sake of simplicity

--- src/main/java/io/github/zam0k/simplifiedpsp/domain/Person.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/domain/Person.java
@@ -1,28 +1,19 @@
 package io.github.zam0k.simplifiedpsp.domain;
 
-import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.UUID;
 
 @NoArgsConstructor
 @Data
 @MappedSuperclass
 public class Person {
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Type(type = "uuid-char")
-    @ColumnDefault("random_uuid()")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false, nullable = false)
-    private UUID id;
+    private Long id;
     @Column(name = "full_name", nullable = false, length = 200)
     private String fullName;
     @Column(nullable = false, unique = true)
