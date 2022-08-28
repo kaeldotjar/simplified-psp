b1a7d7e66ded894cd7ceed0c80825f55609db5e7
Kelly Castelo <kawokkun@gmail.com>
2022-08-28 08:56:46 -0300

    create transaction entity

--- src/main/java/io/github/zam0k/simplifiedpsp/domain/Transaction.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/domain/Transaction.java
@@ -1,2 +1,40 @@
-package io.github.zam0k.simplifiedpsp.domain;public class Transaction {
+package io.github.zam0k.simplifiedpsp.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transaction")
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payer_id", nullable = false)
+    private Person payer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payee_id", nullable = false)
+    private Person payee;
+
+    @Column(name = "`VALUE`", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime timestamp;
+
+    @PrePersist
+    private void createdAt() {
+        this.timestamp = LocalDateTime.now();
+    }
 }
