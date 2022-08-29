a24fbe33e52dca9c7e5fc4801bc3e2eea7ed9c66
Kelly Castelo <kawokkun@gmail.com>
2022-08-29 14:02:15 -0300

    create mapper config

---
+++ src/main/java/io/github/zam0k/simplifiedpsp/config/MapperConfig.java
@@ -0,0 +1,15 @@
+package io.github.zam0k.simplifiedpsp.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+}
