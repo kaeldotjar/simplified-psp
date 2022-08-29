77c98939f0c5b87db61705cd09f739081eb0320a
Kelly Castelo <kawokkun@gmail.com>
2022-08-29 14:05:15 -0300

    change inheritance for composition in juridical person class

--- src/main/java/io/github/zam0k/simplifiedpsp/domain/JuridicalPerson.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/domain/JuridicalPerson.java
@@ -1,17 +1,48 @@
 package io.github.zam0k.simplifiedpsp.domain;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "juridical_person")
-@Data
+@Getter @Setter @EqualsAndHashCode
 @NoArgsConstructor
-public class JuridicalPerson extends Person {
+public class JuridicalPerson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+    @Column(name = "full_name", nullable = false, length = 200)
+    private String fullName;
     @Column(nullable = false, unique = true, length = 14)
     private String cnpj;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String password;
+    @Column(nullable = false)
+    private BigDecimal balance;
+//    @OneToMany(mappedBy="payer")
+//    private List<Transaction> payments;
+//    @OneToMany(mappedBy="payee")
+//    private List<Transaction> receipts;
+
+    @ManyToMany(mappedBy = "shops")
+    private List<NaturalPerson> owners;
+
+    public void addOwner(NaturalPerson person) {
+        owners.add(person);
+        person.getShops().add(this);
+    }
+
+    public void addOwners(List<NaturalPerson> person) {
+        owners.addAll(person);
+        person.forEach(p -> p.getShops().add(this));
+    }
+
 }
