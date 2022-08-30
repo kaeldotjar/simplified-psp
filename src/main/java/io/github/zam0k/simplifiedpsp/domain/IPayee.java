c7ae0c38904a1462873defaa07e33439e78b227f
Kelly Castelo <kawokkun@gmail.com>
2022-08-30 10:25:20 -0300

    create payee and payer interfaces

---
+++ src/main/java/io/github/zam0k/simplifiedpsp/domain/IPayee.java
@@ -0,0 +1,7 @@
+package io.github.zam0k.simplifiedpsp.domain;
+
+import java.math.BigDecimal;
+
+public interface IPayee {
+    void receiveValue(BigDecimal value);
+}
---
+++ src/main/java/io/github/zam0k/simplifiedpsp/domain/IPayer.java
@@ -0,0 +1,7 @@
+package io.github.zam0k.simplifiedpsp.domain;
+
+import java.math.BigDecimal;
+
+public interface IPayer {
+    void removeValue(BigDecimal value);
+}
