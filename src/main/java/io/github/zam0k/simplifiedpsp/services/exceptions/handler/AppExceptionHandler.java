268d3ebe9a83f0abfc6bbb8301a5b65a0d894cb8
Kelly Castelo <kawokkun@gmail.com>
2022-08-31 08:39:44 -0300

    Fetch external authorization api and handle it in transaction service

--- src/main/java/io/github/zam0k/simplifiedpsp/services/exceptions/handler/AppExceptionHandler.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/services/exceptions/handler/AppExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.github.zam0k.simplifiedpsp.services.exceptions.handler;
 
 import io.github.zam0k.simplifiedpsp.services.exceptions.BadRequestException;
+import io.github.zam0k.simplifiedpsp.services.exceptions.BadGatewayException;
 import io.github.zam0k.simplifiedpsp.services.exceptions.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,11 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class AppExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> internalServerErrorException(Exception ex, HttpServletRequest request) {
+        ApiError error = new ApiError(
+                LocalDateTime.now(),
+                INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(error);
+    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiError> notFoundException(NotFoundException ex, HttpServletRequest request) {
@@ -33,5 +42,15 @@ public class AppExceptionHandler {
                 ex.getMessage(),
                 request.getRequestURI());
         return ResponseEntity.status(BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(BadGatewayException.class)
+    public ResponseEntity<ApiError> badGatewayException(BadGatewayException ex, HttpServletRequest request) {
+        ApiError error = new ApiError(
+                LocalDateTime.now(),
+                BAD_GATEWAY.value(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(BAD_GATEWAY).body(error);
     }
 }
--- src/main/java/io/github/zam0k/simplifiedpsp/services/impl/TransactionServiceImpl.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/services/impl/TransactionServiceImpl.java
@@ -8,16 +8,22 @@ import io.github.zam0k.simplifiedpsp.repositories.JuridicalPersonRepository;
 import io.github.zam0k.simplifiedpsp.repositories.NaturalPersonRepository;
 import io.github.zam0k.simplifiedpsp.repositories.TransactionRepository;
 import io.github.zam0k.simplifiedpsp.services.TransactionService;
+import io.github.zam0k.simplifiedpsp.services.exceptions.BadGatewayException;
 import io.github.zam0k.simplifiedpsp.services.exceptions.BadRequestException;
 import io.github.zam0k.simplifiedpsp.services.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.stream.Stream;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +33,7 @@ public class TransactionServiceImpl implements TransactionService {
     private final NaturalPersonRepository naturalPersonRepository;
     private final JuridicalPersonRepository juridicalPersonRepository;
     private final ModelMapper mapper;
+    private final RestTemplate restTemplate;
 
     @Override
     @Transactional
@@ -44,7 +51,26 @@ public class TransactionServiceImpl implements TransactionService {
         payee.receiveValue(value);
         payer.removeValue(value);
 
+        authorizeTransaction();
+
         return repository.save(transaction);
+    }
+
+    private void authorizeTransaction() {
+        String externalAuthorizerServiceURL =
+                "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
+
+        ResponseEntity<String> response;
+
+        try {
+            response = restTemplate
+                    .getForEntity(externalAuthorizerServiceURL, String.class);
+        } catch (RestClientException e) {
+            throw new BadGatewayException("External authorizer service currently unavailable");
+        }
+
+        // TO-DO: check if there's a more fitting error for this
+        if(response.getStatusCode() != OK) throw new BadRequestException("Transaction rejected");
     }
 
     private IPayee getPayee(Transaction transaction) {
