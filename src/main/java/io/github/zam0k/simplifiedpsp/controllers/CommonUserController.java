fae8b8aece6a423a9dd8205bd09f2e8f127a2a54
Kelly Castelo <kawokkun@gmail.com>
2022-09-04 10:14:47 -0300

    Change common-users endpoint name from singular to plural

--- src/main/java/io/github/zam0k/simplifiedpsp/controllers/CommonUserController.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/controllers/CommonUserController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/common-user")
+@RequestMapping("/common-users")
 @RequiredArgsConstructor
 public class CommonUserController {
     private final CommonUserService service;
