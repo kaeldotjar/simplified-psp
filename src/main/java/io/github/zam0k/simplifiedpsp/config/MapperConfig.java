b05b067d8de20c3b508ec0088d804b53d083acb4
Kelly Castelo <kawokkun@gmail.com>
2022-09-04 12:49:29 -0300

    Create beans to handle mismatched type for CommonUser and ShopkeeperUser

--- src/main/java/io/github/zam0k/simplifiedpsp/config/MapperConfig.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/config/MapperConfig.java
@@ -1,5 +1,9 @@
 package io.github.zam0k.simplifiedpsp.config;
 
+import io.github.zam0k.simplifiedpsp.controllers.dto.CommonUserDTO;
+import io.github.zam0k.simplifiedpsp.controllers.dto.ShopkeeperUserDTO;
+import io.github.zam0k.simplifiedpsp.domain.CommonUser;
+import io.github.zam0k.simplifiedpsp.domain.ShopkeeperUser;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +11,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MapperConfig {
 
+    ModelMapper modelMapper;
+
+    public MapperConfig() {
+        modelMapper = new ModelMapper();
+    }
+
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
+    }
+
+    @Bean
+    public void configureCommonUser() {
+        modelMapper.createTypeMap(CommonUser.class, CommonUserDTO.class)
+                .addMapping(CommonUser::getId, CommonUserDTO::setKey);
+
+        modelMapper.createTypeMap(CommonUserDTO.class, CommonUser.class)
+                .addMapping(CommonUserDTO::getKey, CommonUser::setId);
+    }
+
+    @Bean
+    public void configureShopkeeperUser() {
+        modelMapper.createTypeMap(ShopkeeperUser.class, ShopkeeperUserDTO.class)
+                .addMapping(ShopkeeperUser::getId, ShopkeeperUserDTO::setKey);
+
+        modelMapper.createTypeMap(ShopkeeperUserDTO.class, ShopkeeperUser.class)
+                .addMapping(ShopkeeperUserDTO::getKey, ShopkeeperUser::setId);
     }
 }
