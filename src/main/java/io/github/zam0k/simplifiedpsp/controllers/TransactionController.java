b8eb415a733c744e5fa5642a7ec2fe02ff971b51
Kelly Castelo <kawokkun@gmail.com>
2022-09-04 10:17:50 -0300

    Change DI from attributes to constructor and change transctions endpoint from singular to plural

--- src/main/java/io/github/zam0k/simplifiedpsp/controllers/TransactionController.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/controllers/TransactionController.java
@@ -3,8 +3,6 @@ package io.github.zam0k.simplifiedpsp.controllers;
 import io.github.zam0k.simplifiedpsp.controllers.dto.TransactionDTO;
 import io.github.zam0k.simplifiedpsp.services.TransactionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,20 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
 
     private final TransactionService service;
 
-    @Autowired
-    private ApplicationEventPublisher publisher;
-
     @PostMapping
-    public ResponseEntity<TransactionDTO> create(@RequestBody TransactionDTO transaction) {
+    public ResponseEntity<TransactionDTO> create(@Valid @RequestBody TransactionDTO transaction) {
         TransactionDTO newEntity = service.create(transaction);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(newEntity.getId()).toUri();
