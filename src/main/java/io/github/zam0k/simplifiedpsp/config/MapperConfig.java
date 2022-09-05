74f8a8e4de4a9f1a8cd5ff4d090f68b2443acf03
Kelly Castelo <kawokkun@gmail.com>
2022-09-05 09:28:18 -0300

    Create bean to handle mismatched type for Transaction

--- src/main/java/io/github/zam0k/simplifiedpsp/config/MapperConfig.java
+++ src/main/java/io/github/zam0k/simplifiedpsp/config/MapperConfig.java
@@ -2,8 +2,10 @@ package io.github.zam0k.simplifiedpsp.config;
 
 import io.github.zam0k.simplifiedpsp.controllers.dto.CommonUserDTO;
 import io.github.zam0k.simplifiedpsp.controllers.dto.ShopkeeperUserDTO;
+import io.github.zam0k.simplifiedpsp.controllers.dto.TransactionDTO;
 import io.github.zam0k.simplifiedpsp.domain.CommonUser;
 import io.github.zam0k.simplifiedpsp.domain.ShopkeeperUser;
+import io.github.zam0k.simplifiedpsp.domain.Transaction;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +25,7 @@ public class MapperConfig {
     }
 
     @Bean
-    public void configureCommonUser() {
+    public void configureCommonUserMapper() {
         modelMapper.createTypeMap(CommonUser.class, CommonUserDTO.class)
                 .addMapping(CommonUser::getId, CommonUserDTO::setKey);
 
@@ -32,11 +34,20 @@ public class MapperConfig {
     }
 
     @Bean
-    public void configureShopkeeperUser() {
+    public void configureShopkeeperUserMapper() {
         modelMapper.createTypeMap(ShopkeeperUser.class, ShopkeeperUserDTO.class)
                 .addMapping(ShopkeeperUser::getId, ShopkeeperUserDTO::setKey);
 
         modelMapper.createTypeMap(ShopkeeperUserDTO.class, ShopkeeperUser.class)
                 .addMapping(ShopkeeperUserDTO::getKey, ShopkeeperUser::setId);
+    }
+
+    @Bean
+    public void configureTransactionMapper() {
+        modelMapper.createTypeMap(Transaction.class, TransactionDTO.class)
+                .addMapping(Transaction::getId, TransactionDTO::setKey);
+
+        modelMapper.createTypeMap(TransactionDTO.class, Transaction.class)
+                .addMapping(TransactionDTO::getKey, Transaction::setId);
     }
 }
